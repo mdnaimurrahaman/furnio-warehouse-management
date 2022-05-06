d9674147796f22406c80eb92a1ed8db13b969b34
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-05-06 20:39:22 +0600

    To add new items to tha data , the add item page, route and new section hhas been added to the project

--- src/Pages/Home/AddItems/AddITems.js
+++ src/Pages/Home/AddItems/AddITems.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import './AddItems.css'
+import React from "react";
+import { useForm } from "react-hook-form";
+import "./AddItems.css";
 
 const AddITems = () => {
-    return (
-        <div>
-            <h1>This is add Items</h1>
-        </div>
-    );
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
+  return (
+    <div className="login-container">
+      <div className='login-title'>Item Detail</div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+      <input placeholder="Item Name" {...register("Name", { required: true, maxLength: 20 })} />
+      <input placeholder="Item description" {...register("description")} />
+      <input placeholder="Item price" type="number" {...register("price")} />
+      <input placeholder="Item quantity" type="number" {...register("quantity")} />
+      <input placeholder="Supplier Name" type="Name" {...register("name")} />
+      <button type="submit" >Add Item</button>
+    </form>
+    </div>
+  );
 };
 
 export default AddITems;
--- src/Pages/ManageItems/ManageItems.js
+++ src/Pages/ManageItems/ManageItems.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import useItems from '../../Hooks/useItems';
 import AllItems from '../AllItems/AllItems';
 import './ManageItems.css'
@@ -14,9 +15,13 @@ const ManageItems = () => {
                         items.map(item => <AllItems key={item._id} item={item}></AllItems>)
                     }
                 </div>
+                <div className='add-btn d-flex justify-content-center mt-4'>
+                    <Link className='text-white fw-bold pe-auto text-decoration-none' to="/addItems"><button className='text-center mt-3'>Add Items</button></Link>
+                </div>
             </div>
         </div>
     );
 };
 
 export default ManageItems;
+
